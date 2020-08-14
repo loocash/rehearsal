84c36b9826206c4b0d8466129213e3514aba097d
loocash <lukaszkapica@gmail.com>
2020-08-14 17:51:57 +0200

    Add tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1 className="text-6xl text-center text-blue-600 font-bold">App</h1>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import "./styles/tailwind.output.css";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
